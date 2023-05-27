cbaac44ac5c5da5e0752d0bb85aabca90a6313d0
coderick137 <ricardoanj.14@gmail.com>
2023-05-27 17:49:54 -0300

    feat: add compents in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from '@/components/Header'
+import { ProductList } from '@/components/ProductList'
+import { TabMenu } from '@/components/TabMenu'
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <TabMenu />
+      <ProductList />
     </div>
   )
 }
